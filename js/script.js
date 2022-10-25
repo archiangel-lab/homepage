85deb1f0545d53cefbed5732f2e65bd153e5ae62
Angelika Stano <archiangel809@gmail.com>
2022-10-25 22:53:24 +0200

    colors changes

--- js/script.js
+++ js/script.js
@@ -5,21 +5,18 @@
 
     const toggleBacground = () => {
         const body = document.documentElement;
-        // const myNameIs = document.querySelector(".js-nextColorName")
         const nextColorName = document.querySelector(".js-nextColorName")
 
         body.classList.toggle("dark");
-        // myNameIs.classList.toggle("dark");
         nextColorName.innerText = body.classList.contains("dark") ? "JASNY" : "CIEMNY";
     };
 
 
     const init = () => {
         const changeBacgroundButton = document.querySelector(".js-changeBacgroundButton");
-        // const myNameIs = document.querySelector(".myNameIs" toggleBacground);
         changeBacgroundButton.addEventListener("click", toggleBacground);
 
         welcome();
     };
-        init();
-    }
+    init();
+}
