fd0eaae923c24400053219127a6512432772dc49
Angelika Stano <archiangel809@gmail.com>
2022-10-25 21:45:10 +0200

    arrow functions added

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
-console.log("Witaj na mojej stronie! Proszę się rozgościć 😃 Może kawy? ☕");
+{
+    const welcome = () => {
+        console.log("Witaj na mojej stronie! Proszę się rozgościć 😃 Może kawy? ☕");
+    }
+
+    const toggleBacground = () => {
+        const body = document.documentElement;
+        // const myNameIs = document.querySelector(".js-nextColorName")
+        const nextColorName = document.querySelector(".js-nextColorName")
+
+        body.classList.toggle("dark");
+        // myNameIs.classList.toggle("dark");
+        nextColorName.innerText = body.classList.contains("dark") ? "JASNY" : "CIEMNY";
+    };
 
 
+    const init = () => {
+        const changeBacgroundButton = document.querySelector(".js-changeBacgroundButton");
+        // const myNameIs = document.querySelector(".myNameIs" toggleBacground);
+        changeBacgroundButton.addEventListener("click", toggleBacground);
 
-let changeBacgroundButton = document.querySelector(".js-changeBacgroundButton");
-let body = document.querySelector(".body");
-let nextColorName = document.querySelector(".js-nextColorName");
-let welcome = document.querySelector(".welcome");
-
-
-changeBacgroundButton.addEventListener("click", () => {
-    body.classList.toggle("dark");
-    welcome.classList.toggle("dark");
-    nextColorName.innerText = body.classList.contains("dark") ? "JASNY" : "CIEMNY";
-
-
-});
-
-
+        welcome();
+    };
+        init();
+    }
